ff5913e5bbcb9e25e7a23aec8bf41ea32aa80e79
Ascen Keeprov <assenkiprov@yahoo.com>
2017-12-03 22:32:24 +0200

    Exercise10 upload

---
+++ Exercise10-Spring/CalculatorJAVA/src/main/java/calculator/entity/Calculator.java
@@ -0,0 +1,58 @@
+package calculator.entity;
+
+public class Calculator {
+    private double leftOperand;
+
+    public double getLeftOperand() {
+        return leftOperand;
+    }
+
+    public void setLeftOperand(double leftOperand) {
+        this.leftOperand = leftOperand;
+    }
+
+    private String operator;
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    private double rightOperand;
+
+    public double getRightOperand() {
+        return rightOperand;
+    }
+
+    public void setRightOperand(double rightOperand) {
+        this.rightOperand = rightOperand;
+    }
+
+    public Calculator(double leftOperand, String operator, double rightOperand) {
+        this.leftOperand = leftOperand;
+        this.operator = operator;
+        this.rightOperand = rightOperand;
+    }
+
+    public double calculateResult() {
+        double result = 0;
+        switch (this.operator) {
+            case "+":
+                result = this.getLeftOperand() + this.getRightOperand();
+                break;
+            case "-":
+                result = this.getLeftOperand() - this.getRightOperand();
+                break;
+            case "*":
+                result = this.getLeftOperand() * this.getRightOperand();
+                break;
+            case "/":
+                result = this.getLeftOperand() / this.getRightOperand();
+                break;
+        }
+        return result;
+    }
+}
